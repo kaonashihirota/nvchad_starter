e7f764a7c3bc94b2daed7d792a56acbd2c297177
kah <kah@triki9e.dev>
2024-11-13 00:00:29 +0800

    change style.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,8 +5,6 @@
 ---@type ChadrcConfig
 local M = {}
 
-local statusline_bg = '#333945'
-
 M.base46 = {
 	theme = "nord",
 
@@ -22,18 +20,21 @@ M.base46 = {
     St_cwd_sep = { fg = "#3f4551", bg = "#3f4551" },
     St_pos_sep = { fg = "#3f4551", bg = "#3f4551" },
     St_pos_icon = { fg = "#646a76", bg = "#3f4551" },
-    St_NormalMode = { bg = "#4b515d" },
-    St_NormalModeSep = { fg = "#4b515d", bg = "#4b515d"},
+    St_NormalMode = { bg = "#646a76" },
+    St_NormalModeSep = { fg = "#646a76", bg = "#646a76"},
     St_VisualMode = {
-      bg = "#4b515d"
+      bg = "#646a76"
     },
-    St_VisualModeSep = { fg = "#4b515d", bg = "#4b515d"},
-    St_InsertMode = { bg = "#4b515d" },
-    St_InsertModeSep = { fg = "#4b515d", bg = "#4b515d"},
-    St_CommandMode = { bg = "#4b515d" },
-    St_CommandModeSep = { fg = "#4b515d", bg = "#4b515d"},
+    St_VisualModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_InsertMode = { bg = "#646a76" },
+    St_InsertModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_CommandMode = { bg = "#646a76" },
+    St_CommandModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_EmptySpace = { fg = "#646a76" },
+
+
     NvimTreeGitDirty = {
-    fg = "#a983a2"
+      fg = "#a983a2"
     },
     NvimTreeGitNew = {
       fg = "#88C0D0",
