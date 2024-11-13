d17e5bd4e2a8e58240ac53bf29e74ebd87c2cdc8
kah <kah@triki9e.dev>
2024-11-13 11:21:05 +0800

    change term style

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "nord",
+  theme = "nord",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
   hl_override = {
     St_cwd_icon = {
       fg = "#646a76",
@@ -22,25 +22,55 @@ M.base46 = {
     St_pos_icon = { fg = "#646a76", bg = "#3f4551" },
     St_pos_text = { fg = "#6484a4", bg = "#3f4551" },
     St_NormalMode = { bg = "#646a76" },
-    St_NormalModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_NormalModeSep = { fg = "#646a76", bg = "#646a76" },
     St_VisualMode = {
-      bg = "#646a76"
+      bg = "#646a76",
     },
-    St_VisualModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_VisualModeSep = { fg = "#646a76", bg = "#646a76" },
     St_InsertMode = { bg = "#646a76" },
-    St_InsertModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_InsertModeSep = { fg = "#646a76", bg = "#646a76" },
     St_CommandMode = { bg = "#646a76" },
-    St_CommandModeSep = { fg = "#646a76", bg = "#646a76"},
+    St_CommandModeSep = { fg = "#646a76", bg = "#646a76" },
+    St_TerminalMode = {
+      bg = "#646a76",
+    },
+    St_TerminalModeSep = { fg = "#646a76", bg = "#646a76" },
     St_EmptySpace = { fg = "#646a76" },
     St_Lsp = { fg = "#6484a4" },
 
     NvimTreeGitDirty = {
-      fg = "#a983a2"
+      fg = "#a983a2",
     },
     NvimTreeGitNew = {
       fg = "#88C0D0",
     },
-  }
+    NvimTreeWindowPicker = {
+      fg = "#88C0D0",
+    },
+    WhichKeyDesc = {
+      fg = "#A3BE8C",
+    },
+    WhichKeyGroup = {
+      fg = "#7797b7",
+    },
+    TelescopePromptTitle = {
+      bg = "NONE",
+      fg = "#7797b7",
+    },
+    TelescopePreviewTitle = {
+      bg = "NONE",
+      fg = "#7797b7",
+    },
+    TelescopePromptPrefix = {
+      fg = "#6484a4",
+    },
+  },
+}
+
+M.ui = {
+  tabufline = {
+    enabled = false,
+  },
 }
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,21 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+map("n", "<leader>v",
+  function()
+        require("nvchad.term").toggle({ pos="float" })
+  end,
+  { desc= "toggle floating term"}
+)
+map("n", "<leader>h", "<cmd>wincmd h<CR>", { desc = "h" })
+map("n", "<leader>j", "<cmd>wincmd j<CR>", { desc = "j" })
+map("n", "<leader>k", "<cmd>wincmd k<CR>", { desc = "k" })
+map("n", "<leader>l", "<cmd>wincmd l<CR>", { desc = "l" })
+map("n", "<leader>=", "<cmd>wincmd =<CR>", { desc = "=" })
+
+map("n", "<Esc><Esc>", ":w<cr>", { desc = "save" })
+map("n", "<leader>ww", ":w<cr>", { desc = "save" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
