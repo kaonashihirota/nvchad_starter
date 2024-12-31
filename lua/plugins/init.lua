fb32eade6b460b6069e5fc62eced74d5f6e79123
kah <kah@triki9e.dev>
2024-12-31 16:44:11 +0800

    chore(plugins): indent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -248,7 +248,25 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
+    ---
     opts = {},
     event = "VeryLazy",
+    config = function()
+      require("render-markdown").setup {
+        -- Mimic org-indent-mode behavior by indenting everything under a heading based on the
+        -- level of the heading. Indenting starts from level 2 headings onward.
+        indent = {
+          -- Turn on / off org-indent-mode
+          enabled = true,
+          -- Amount of additional padding added for each heading level
+          per_level = 2,
+          -- Heading levels <= this value will not be indented
+          -- Use 0 to begin indenting from the very first level
+          skip_level = 1,
+          -- Do not indent heading titles, only the body
+          skip_heading = false,
+        },
+      }
+    end,
   },
 }
