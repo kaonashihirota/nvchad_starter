a4624d5edce4665d7f12c2e2925ffc0be9497ea9
kah <kah@triki9e.dev>
2025-01-01 15:16:26 +0800

    chore(plugin): add image.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -269,4 +269,15 @@ return {
       }
     end,
   },
+  {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {},
+    event = 'VeryLazy',
+    config = function()
+      require("image").setup({
+        processor = "magick_cli", -- or "magick_cli"
+      })
+    end,
+  }
 }
