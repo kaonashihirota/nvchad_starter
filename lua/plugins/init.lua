096340992f07821c92b2dac645bd9c88f12bed51
kah <kah@triki9e.dev>
2025-01-01 16:36:25 +0800

    chore(plugins): add avante.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -266,6 +266,18 @@ return {
           -- Do not indent heading titles, only the body
           skip_heading = false,
         },
+        html = {
+          -- Turn on / off all HTML rendering
+          enabled = true,
+          comment = {
+            -- Turn on / off HTML comment concealing
+            conceal = true,
+            -- Optional text to inline before the concealed comment
+            text = nil,
+            -- Highlight for the inlined text
+            highlight = "RenderMarkdownHtmlComment",
+          },
+        },
       }
     end,
   },
@@ -273,11 +285,70 @@ return {
     "3rd/image.nvim",
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {},
-    event = 'VeryLazy',
+    event = "VeryLazy",
     config = function()
-      require("image").setup({
+      require("image").setup {
         processor = "magick_cli", -- or "magick_cli"
-      })
+      }
     end,
-  }
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  config = function()
+    require("avante").setup {
+      provider = "openai",
+      auto_suggestions_provider = "openai",
+      dual_boost = {
+        enabled = true,
+        first_provider = "openai",
+        second_provider = "claude",
+        prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+        timeout = 60000, -- Timeout in milliseconds
+      },
+    }
+  end,
 }
