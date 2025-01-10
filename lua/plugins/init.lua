6fcb37647ed6f8cacdb48f5f19144e476e6c6ab7
kah <kah@triki9e.dev>
2025-01-10 15:59:57 +0800

    chore(sync): sync

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -113,6 +113,7 @@ return {
                 lab = "~/neorg_notes/lab",
                 stack = "~/neorg_notes/stack",
                 chore = "~/neorg_notes/chore",
+                triki9e = "~/neorg_notes/triki9e",
                 -- dev = "~/dev",
               },
             },
