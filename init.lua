10baa1e70cf5452a083821f60c9108127d27aa50
kah <kah@triki9e.dev>
2025-01-22 11:36:02 +0800

    chore(sync): sync

--- init.lua
+++ init.lua
@@ -58,11 +58,11 @@ vim.api.nvim_set_hl(0, 'RenderMarkdownBullet', { link = "RenderMarkdownH1Bg" })
 vim.api.nvim_set_hl(0, 'RenderMarkdownQuote', { link = "RenderMarkdownH1Bg" })
 vim.api.nvim_set_hl(0, 'AvanteThirdTitle', { link = "RenderMarkdownH1Bg" })
 
-vim.cmd[[highlight AvanteTitle ctermbg=0 guibg=#4b515d guifg=#2E3440]]
-vim.cmd[[highlight AvanteReversedTitle ctermbg=0 guifg=#4b515d]]
+vim.cmd[[highlight AvanteTitle ctermbg=0 guibg=#646a76 guifg=#2E3440 gui=bold]]
+vim.cmd[[highlight AvanteReversedTitle ctermbg=0 guifg=#646a76]]
 
-vim.cmd[[highlight AvanteSubtitle ctermbg=0 guibg=#4b515d guifg=#2E3440]]
-vim.cmd[[highlight AvanteReversedSubtitle ctermbg=0 guifg=#4b515d]]
+vim.cmd[[highlight AvanteSubtitle ctermbg=0 guibg=#646a76 guifg=#2E3440 gui=bold]]
+vim.cmd[[highlight AvanteReversedSubtitle ctermbg=0 guifg=#646a76]]
 
 vim.api.nvim_set_hl(0, 'AvanteInlineHint', { link = "Comment" })
 vim.api.nvim_set_hl(0, 'AvantePopupHint', { link = "Comment" })
